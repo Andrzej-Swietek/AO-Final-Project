e610927cf8fe1c512670c7ab6f3a9424231dcb5f
Andrzej Świętek <mr.andi321@gmail.com>
2025-01-23 19:17:44 +0100

    Fixed frontend status

--- frontend/src/routes/UploadPage.tsx
+++ frontend/src/routes/UploadPage.tsx
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 
+type OperationStatus =
+    'Finished'
+    | 'Idle'
+    | 'Uploading...'
+    | 'Failed'
+    | 'Processing...';
+
 export const UploadPage = () => {
-  const [status, setStatus] = useState("Idle");
+  const [status, setStatus] = useState<OperationStatus>("Idle");
   const [contentImage, setContentImage] = useState<File | null>(null);
   const [contentImageUrl, setContentImageUrl] = useState<string | null>(null);
   const [resultImages, setResultImages] = useState<string[]>([]);
+  const [taskId, setTaskId] = useState<string>("");
+
   const PUBLIC_API_URL = "http://localhost:5000";
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -14,6 +23,12 @@ export const UploadPage = () => {
       setContentImageUrl(URL.createObjectURL(file));
     }
   };
+
+  const download = async () => {
+    const response = await fetch(`${PUBLIC_API_URL}/api/download/${taskId}`);
+    const data: object = await response.json();
+    console.log(data)
+  }
 
   const uploadImage = async () => {
     if (!contentImage) {
@@ -35,15 +50,15 @@ export const UploadPage = () => {
       if (response.ok) {
         setStatus("Processing...");
         const { task_id } = data;
-
+        setTaskId(task_id);
         // Polling status
         const interval = setInterval(async () => {
           const statusResponse = await fetch(`${PUBLIC_API_URL}/api/task_status/${task_id}`);
           const statusData = await statusResponse.json();
 
-          if (statusData.status === "Completed") {
-            setStatus("Completed");
-            setResultImages(statusData.results); 
+          if (statusData.status === "Finished") {
+            setStatus("Finished");
+            setResultImages([]);
             clearInterval(interval);
           } else if (statusData.status === "Failed") {
             setStatus("Failed");
@@ -74,18 +89,19 @@ export const UploadPage = () => {
             <p className="max-w-[900px] text-muted-foreground md:text-xl font-bold">Status: {status}</p>
           </div>
           <div className="grid grid-cols-2 gap-4 w-full max-w-4xl">
-            <div className="flex flex-col col-span-2 items-center justify-center border-2 border-dashed border-black rounded-lg p-24 space-y-4">
+            <div
+                className="flex flex-col col-span-2 items-center justify-center border-2 border-dashed border-black rounded-lg p-24 space-y-4">
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="w-8 h-8 text-muted-foreground"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="w-8 h-8 text-muted-foreground"
               >
                 <rect width="18" height="18" x="3" y="3" rx="2" ry="2"></rect>
                 <circle cx="9" cy="9" r="2"></circle>
@@ -93,38 +109,61 @@ export const UploadPage = () => {
               </svg>
               <p className="text-sm text-muted-foreground mb-8">Upload Image</p>
               <input
-                className="flex h-10 mt-4 font-semibold rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full"
-                placeholder="Upload image"
-                accept="image/png, image/jpeg"
-                type="file"
-                onChange={handleFileChange}
+                  className="flex h-10 mt-4 font-semibold rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full"
+                  placeholder="Upload image"
+                  accept="image/png, image/jpeg"
+                  type="file"
+                  onChange={handleFileChange}
               />
             </div>
             {contentImageUrl && (
-              <div className="flex flex-col items-center justify-center gap-4 mt-8 col-span-2">
-                <p className="text-muted-foreground md:text-xl">Image preview:</p>
-                <img className="w-[300px] h-[300px] object-cover rounded-md" src={contentImageUrl} alt="Preview" />
-              </div>
+                <div className="flex flex-col items-center justify-center gap-4 mt-8 col-span-2">
+                  <p className="text-muted-foreground md:text-xl">Image preview:</p>
+                  <img className="w-[300px] h-[300px] object-cover rounded-md" src={contentImageUrl} alt="Preview"/>
+                </div>
             )}
           </div>
-          <button
-            className="inline-flex text-xl items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-32 py-2 mt-16"
-            type="button"
-            onClick={uploadImage}
-          >
-            Process Image
-          </button>
+
+          {
+              status == 'Finished' ? (
+                  <>
+                    <button
+                        className="inline-flex text-xl items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-32 py-2 mt-16"
+                        type="button"
+                        onClick={() => download()}
+                    >
+                      Download
+                    </button>
+                    <button
+                        className="inline-flex text-xl items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-32 py-2 mt-16"
+                        type="button"
+                        onClick={()=> window.location.reload()}
+                    >
+                      Reset
+                    </button>
+                  </>
+              ) : (
+                  <button
+                      className="inline-flex text-xl items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-32 py-2 mt-16"
+                      type="button"
+                      onClick={uploadImage}
+                  >
+                    Process Image
+                  </button>
+              )
+          }
         </div>
+
         <div className="mt-8">
           {resultImages.length > 0 && (
-            <>
-              <h2 className="text-2xl font-bold mt-4">Results</h2>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                {resultImages.map((image, idx) => (
-                  <img key={idx} src={image} alt="Segmented and Colorized" className="w-full h-auto" />
-                ))}
-              </div>
-            </>
+              <>
+                <h2 className="text-2xl font-bold mt-4">Results</h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                  {resultImages.map((image, idx) => (
+                      <img key={idx} src={image} alt="Segmented and Colorized" className="w-full h-auto"/>
+                  ))}
+                </div>
+              </>
           )}
         </div>
       </div>
